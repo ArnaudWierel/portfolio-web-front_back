c33db7ab93ba47667abe67a14f3d6787c61c45d8
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2024-02-21 16:27:56 +0100

    Mise à jour de l'apparence du site et ajout de la page de maintenance

--- portfolio-arnaud-wierel/src/App.vue
+++ portfolio-arnaud-wierel/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+
   <router-view/>
 </template>
 
@@ -36,17 +33,15 @@
         }
 
 #app {
-  font-family: Tuseker Grotesk, Neue Montreal, Migra, Maelstrom, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 h1{
-  font-family: Truseker Grotesk, sans-serif;
-  font-size: 3em;
+  font-family: Tusker Grotesk, sans-serif;
   font-weight: 900;
+  font-size: 8em;
   color: #2c3e50;
 
 }
--- portfolio-arnaud-wierel/src/views/HomeView.vue
+++ portfolio-arnaud-wierel/src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.svg">
+    <MsgMaintenance msg="Ce site est en construction"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MsgMaintenance from '@/components/Maintenance.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MsgMaintenance
   }
 }
 </script>
+
+<style scoped>
+img {
+  width: 200px;
+  height: 200px;
+  margin-bottom: 50px;
+}
+
+.home {
+  background-image: url("../assets/images/maintenance-background.jpg");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
