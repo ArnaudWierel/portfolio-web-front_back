4ec4edf03dc627fed824b8dcdfd6f4d3d14007c1
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2024-02-21 14:58:35 +0100

    Ajouter des polices personnalisées et styliser les titres

--- portfolio-arnaud-wierel/src/App.vue
+++ portfolio-arnaud-wierel/src/App.vue
@@ -7,12 +7,48 @@
 </template>
 
 <style>
+@font-face {
+            font-family: 'Tusker Grotesk';
+            src: url('./assets/font/TuskerGroteskFont/TuskerGrotesk-3500Medium.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'Neue Montreal';
+            src: url('./assets/font/NeueMontreal/NeueMontreal-Regular.otf') format('opentype');
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'Migra';
+            src: url('./assets/font/Migra/Migra-Extralight.woff2') format('woff2');
+            font-weight: 600;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'Maelstrom';
+            src: url('./assets/font/Maelstrom/MaelstromBold.ttf') format('truetype');
+            font-weight: bold;
+            font-style: normal;
+        }
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Tuseker Grotesk, Neue Montreal, Migra, Maelstrom, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+h1{
+  font-family: Truseker Grotesk, sans-serif;
+  font-size: 3em;
+  font-weight: 900;
+  color: #2c3e50;
+
 }
 
 nav {
