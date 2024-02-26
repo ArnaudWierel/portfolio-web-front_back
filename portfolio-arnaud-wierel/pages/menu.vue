31a15ec46096c9c1d3f2c451d3aebb80369deca8
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2024-02-26 09:38:24 +0100

    Ajout d'une icône de menu et d'une fonctionnalité de basculement du menu

--- portfolio-arnaud-wierel/pages/menu.vue
+++ portfolio-arnaud-wierel/pages/menu.vue
@@ -1,8 +1,19 @@
 <template>
 <div class="menu">
+    <Icon name="line-md:menu-to-close-alt-transition" size="50" @click="toggleMenu"/>
     <div class="message">
         <h1>Menu</h1>
         <p>Click on the links to navigate</p>
     </div>
 </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        toggleMenu() {
+            this.$router.push('/');
+        },
+    },
+};
+</script>
