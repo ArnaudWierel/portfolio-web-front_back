16ce7f4118b5c7cf301374c659edf3337db118d3
Arnaud <arnaud.67210@gmail.com>
2025-02-11 00:21:18 +0100

    ou on eu encore chaud sur cette branche

--- portfolio-arnaud-wierel/nuxt.config.ts
+++ portfolio-arnaud-wierel/nuxt.config.ts
@@ -1,15 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   typescript: {
     shim: false
   },
+
   css: ['~/assets/css/main.scss'],
+
   modules: [
     '@nuxt/image',
   ],
+
   image: {
     dir: '~/assets/images'
   },
-  
+
+  compatibilityDate: '2025-02-11',
 })
