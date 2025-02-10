e2170f9c0af6d3ff26ee8cff4c9e13135b0602d7
Arnaud <arnaud.67210@gmail.com>
2025-02-11 00:19:19 +0100

    ouf on a eu chaud

--- portfolio-arnaud-wierel/nuxt.config.ts
+++ portfolio-arnaud-wierel/nuxt.config.ts
@@ -1,24 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   typescript: {
     shim: false
   },
+
   css: ['~/assets/css/main.scss'],
+
   modules: [
     '@nuxt/image',
     'nuxt-icon',
     '@hypernym/nuxt-anime',
     '@hypernym/nuxt-gsap'
   ],
+
   image: {
     dir: '~/assets/images'
   },
+
   anime: {
     composables: true
   },
+
   gsap: {
     composables: true,
     provide: false
-  }
+  },
+
+  nitro: {
+    prerender: {
+      ignore: ['/home'] // Exclude `/home` from prerendering
+    }
+  },
+
+  compatibilityDate: '2025-02-11'
 })
